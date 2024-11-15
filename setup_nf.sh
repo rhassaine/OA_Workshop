61dbeb9566d0470dbdbf0c5122a1f11ef451c082
rhassaine <r.hassaine@hartwigmedicalfoundation.nl>
2024-11-15 15:15:31 +0100

    Added steps for installing Docker on Ubuntu

--- setup_nf.sh
+++ setup_nf.sh
@@ -53,15 +53,35 @@ fi
 # Confirm the Java installation
 java -version
 
+# Check and install Docker if not installed
 echo "Checking if Docker is installed..."
+if ! command -v docker >/dev/null; then
+  echo "Docker not found. Installing Docker..."
+  sudo apt-get update
+  sudo apt-get install -y \
+      ca-certificates \
+      curl \
+      gnupg \
+      lsb-release
 
-# Check Docker installation
-if docker_version=$(docker --version); then
-  echo "Docker is installed: $docker_version"
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+  echo \
+    "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+    $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+  sudo apt-get update
+  sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+  sudo systemctl start docker
+  sudo systemctl enable docker
+
+  echo "Docker installed successfully."
 else
-  echo "Docker is not installed. Please install Docker manually and re-run the script."
-  exit 1  # Exit if Docker is not installed, as it is required
+  echo "Docker is already installed: $(docker --version)"
 fi
+
 
 # Install Nextflow
 echo "Installing Nextflow..."
